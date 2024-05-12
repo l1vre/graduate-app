1850ad8c2233ba50be7e1813c53616cd7490e932
l1vre <anotto22@gmail.com>
2024-05-12 20:30:17 +0300

    [NEW] Add DB module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true })]
+	imports: [
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: process.env.DB_HOST,
+			database: process.env.DB_NAME,
+			port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+			username: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			entities: [],
+			autoLoadEntities: true,
+			synchronize: true
+		})
+	]
 })
 export class AppModule {}
