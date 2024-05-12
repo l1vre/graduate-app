c6079f6659b3cc1abeef5d22d1ce1fe8b643e5a0
l1vre <anotto22@gmail.com>
2024-05-12 17:47:17 +0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+(async function () {
+	const PORT = process.env.PORT || 3000;
+	const app = NestFactory.create(AppModule);
+	(await app).listen(PORT, () =>
+		console.log(`[START] PROJECT STARTED!!! -> http://localhost:${PORT}/`)
+	);
+})();
