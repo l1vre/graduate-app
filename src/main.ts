f62d441dc5dc578d15c45c575579d3c7dcf50679
l1vre <anotto22@gmail.com>
2024-05-13 11:02:50 +0300

    [NEW] Add validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 (async function () {
 	const PORT = process.env.PORT || 3000;
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(new ValidationPipe());
 	(await app).listen(PORT, () =>
 		console.log(`[START] PROJECT STARTED!!! -> http://localhost:${PORT}/`)
 	);
