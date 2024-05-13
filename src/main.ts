65811ef93bc3714b34513dd9b502af26f2f12eb3
l1vre <anotto22@gmail.com>
2024-05-13 10:48:14 +0300

    [FIX] Fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 (async function () {
 	const PORT = process.env.PORT || 3000;
-	const app = NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
 	(await app).listen(PORT, () =>
 		console.log(`[START] PROJECT STARTED!!! -> http://localhost:${PORT}/`)
 	);
