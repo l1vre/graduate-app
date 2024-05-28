f12c9d3ed29415c1a525bee6e1ee8930a8106bb0
l1vre <anotto22@gmail.com>
2024-05-28 09:33:34 +0300

    [NEW] Add getAll method for teachers

--- src/teachers/teachers.controller.ts
+++ src/teachers/teachers.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TeachersService } from './teachers.service';
+import { Teacher } from './teacher.entity';
 
 @Controller('teachers')
 export class TeachersController {
 	constructor(private readonly teachersService: TeachersService) {}
+
+	@Get()
+	async getAll(): Promise<Teacher[]> {
+		return await this.teachersService.getAll();
+	}
 }
--- src/teachers/teachers.service.ts
+++ src/teachers/teachers.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Teacher } from './teacher.entity';
+import { EntityManager, Repository } from 'typeorm';
 
 @Injectable()
-export class TeachersService {}
+export class TeachersService {
+	constructor(
+		@InjectRepository(Teacher)
+		private readonly teachersRepository: Repository<Teacher>,
+		private readonly entityManager: EntityManager
+	) {}
+
+	async getAll(): Promise<Teacher[]> {
+		return this.teachersRepository.find();
+	}
+}
